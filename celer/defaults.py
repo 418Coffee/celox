25907e14c2cc660de5b52c12ccf6225d237b071d
418Coffee <matthijskes@gmail.com>
2022-03-16 20:53:16 +0100

    Update defaults.py

--- celer/defaults.py
+++ celer/defaults.py
@@ -1,12 +1,13 @@
-from multidict import MultiDict, MultiDictProxy
-
+from .util import FrozenOrderedDict
 from .timeout import Timeout
 from . import __version__
 
 USER_AGENT: str = f"celer/{__version__}"
 
-DEFAULT_HEADERS: MultiDictProxy = MultiDictProxy(MultiDict({
+DEFAULT_HEADERS: FrozenOrderedDict = FrozenOrderedDict({
+    "Host": "",
     "User-Agent": USER_AGENT,
-}))
+    "Accept": "*/*",
+})
 
 DEFAULT_TIMEOUT: Timeout = Timeout(total=5)
