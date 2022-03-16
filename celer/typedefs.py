144b9d455f4d8a6f94f6577a450d6d660b07456b
418Coffee <matthijskes@gmail.com>
2022-03-16 20:53:09 +0100

    Update typedefs.py

--- celer/typedefs.py
+++ celer/typedefs.py
@@ -1,7 +1,10 @@
 from ssl import SSLContext, SSLObject
 from typing import Dict, OrderedDict, Union
+from multidict import CIMultiDict, CIMultiDictProxy
 from yarl import URL
 from trio import SSLStream, SocketStream
+
+from .util import FrozenOrderedDict, frozendict
 from .timeout import Timeout
 
 StrOrURL = Union[str, URL]
@@ -9,7 +12,7 @@ CookieJar = ...
 ClientTimeout = ...
 
 TimeoutLike = Union[Timeout, float, int]
-DictLike = Union[OrderedDict, dict, Dict]
+DictLike = Union[OrderedDict, dict, frozendict, FrozenOrderedDict, CIMultiDict, CIMultiDictProxy]
 ProxyLike = StrOrURL
 SSLLike = Union[SSLContext, bool]
 StreamLike = Union[SSLStream, SocketStream]
