0aff391dba3063a06e1d4a62e89bcd06b3232bae
418Coffee <matthijskes@gmail.com>
2022-03-17 21:22:24 +0100

    Update typedefs.py

--- celer/typedefs.py
+++ celer/typedefs.py
@@ -1,18 +1,28 @@
-from ssl import SSLContext, SSLObject
-from typing import Dict, OrderedDict, Union
-from multidict import CIMultiDict, CIMultiDictProxy
-from yarl import URL
-from trio import SSLStream, SocketStream
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+from ssl import SSLContext
+from typing import Any, Union
 
-from .util import FrozenOrderedDict, frozendict
+from multidict import CIMultiDict, CIMultiDictProxy
+from trio import SocketStream, SSLStream
+from yarl import URL
+
 from .timeout import Timeout
+from .util import FrozenOrderedDict, frozendict
 
 StrOrURL = Union[str, URL]
-CookieJar = ...
-ClientTimeout = ...
+# TODO:
+# This should get it's own class
+CookieJar = Any
 
 TimeoutLike = Union[Timeout, float, int]
 DictLike = Union[OrderedDict, dict, frozendict, FrozenOrderedDict, CIMultiDict, CIMultiDictProxy]
 ProxyLike = StrOrURL
 SSLLike = Union[SSLContext, bool]
 StreamLike = Union[SSLStream, SocketStream]
+
+class JsonEncoder(ABC):
+    
+    @abstractmethod
+    def dumps(self) -> str:
+        pass
