7fe260fe14d2636c8c1d24b6725fa0bb89cbbcc2
418Coffee <matthijskes@gmail.com>
2022-03-31 17:52:30 +0200

    Update typedefs.py

--- celer/typedefs.py
+++ celer/typedefs.py
@@ -1,7 +1,7 @@
-from abc import ABC, abstractmethod
 from collections import OrderedDict
+from http.cookies import BaseCookie, Morsel
 from ssl import SSLContext
-from typing import Any, Union
+from typing import Any, Callable, Iterable, Mapping, Union
 
 from multidict import CIMultiDict, CIMultiDictProxy
 from trio import SocketStream, SSLStream
@@ -10,19 +10,20 @@ from yarl import URL
 from .timeout import Timeout
 from .util import FrozenOrderedDict, frozendict
 
+# URL
 StrOrURL = Union[str, URL]
-# TODO:
-# This should get it's own class
-CookieJar = Any
 
+# Cookies
+CookieItem = Union[str, "Morsel[str]"]
+ClearCookiePredicate = Callable[["Morsel[str]"], bool]
+LooseCookiesMappings = Mapping[str, Union[str, "BaseCookie[str]", "Morsel[Any]"]]
+LooseCookiesIterables = Iterable['tuple[str, Union[str, "BaseCookie[str]", "Morsel[Any]"]]']
+
+# Likes
 TimeoutLike = Union[Timeout, float, int]
 DictLike = Union[OrderedDict, dict, frozendict, FrozenOrderedDict, CIMultiDict, CIMultiDictProxy]
 ProxyLike = StrOrURL
 SSLLike = Union[SSLContext, bool]
 StreamLike = Union[SSLStream, SocketStream]
+CookieLike = Union[LooseCookiesMappings, LooseCookiesIterables, "BaseCookie[str]"]
 
-class JsonEncoder(ABC):
-    
-    @abstractmethod
-    def dumps(self) -> str:
-        pass
