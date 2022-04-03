28f712ec4dba9db4c21e40204d53df7f36038e31
418Coffee <matthijskes@gmail.com>
2022-04-03 16:05:22 +0200

    Update timeout.py

--- celer/timeout.py
+++ celer/timeout.py
@@ -3,7 +3,7 @@ from typing import Optional, Union
 
 _valid_types = (float, int, type(None))
 
-@attr.s(repr=True, slots=True)
+@attr.s(frozen=True, repr=True, slots=True)
 class Timeout:
     total: Optional[Union[float, int]] = attr.ib(default=None, validator=attr.validators.instance_of(_valid_types))
     connect: Optional[Union[float, int]] = attr.ib(default=None, validator=attr.validators.instance_of(_valid_types))
