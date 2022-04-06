9650b50ab99d372f361dcdc5855a55637830a8ff
418Coffee <matthijskes@gmail.com>
2022-04-06 16:41:05 +0200

    Add proxy user:password support

--- celer/proxy.py
+++ celer/proxy.py
@@ -1,17 +1,26 @@
-from yarl import URL
+import base64
+from typing import Any, Optional
+
+import yarl
 
 from .exceptions import InvalidProxy
 from .typedefs import ProxyLike
 
 
-def _connect_request(host, port) -> bytes:
-    return  f"CONNECT {host}:{port} HTTP/1.1\r\n"   \
-            f"Host: {host}:{port}\r\n\r\n".encode("ascii")
+def _connect_request(host: Any, port: Any, *, user: Optional[Any] = None, password: Optional[Any] = None) -> bytes:
+    request = f"CONNECT {host}:{port} HTTP/1.1\r\n"   \
+            f"Host: {host}:{port}\r\n"
+    if not user and not password:
+        request += "\r\n"
+        return request.encode()
+    auth = base64.b64encode(f"{user}:{password}".encode("ascii"))
+    request += f"Proxy-Authorization: basic {auth.decode('ascii')}\r\n"
+    return request.encode("ascii") 
 
-def _prepare_proxy(proxy: ProxyLike) -> URL:
-    if isinstance(proxy, URL):
+def _prepare_proxy(proxy: ProxyLike) -> yarl.URL:
+    if isinstance(proxy, yarl.URL):
         return proxy
     try:
-        return URL(proxy)
+        return yarl.URL(proxy)
     except ValueError as e:
         raise InvalidProxy(proxy) from e
